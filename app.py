5798bfa2b86b6fd2f9fbf957145c315381e34562
Minakshi Sharma <nityasharma2203@gmail.com>
2025-12-28 22:31:23 +0530

    Update API key placeholder in fetch_poster function

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import requests
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=enter ur api key".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
@@ -53,6 +53,7 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+
 
 
 
